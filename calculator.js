d4ecf138fa57f879e296a393df00ca109188c4cb
Lauryn Thayne <lalder57@gmail.com>
2024-08-01 14:43:59 -0600

    converting to numbers

--- calculator.js
+++ calculator.js
@@ -2,56 +2,96 @@
 
 // Addition operation
 function addNums(num1, num2) {
-  console.log(Number(num1) + Number(num2));
+  return (Number(num1) + Number(num2));
 }
 
 addNums("1", 2);
 
 // Subraction operation
 function subtractNums(num1, num2) {
-  console.log(Number(num1) - Number(num2));
+  return (Number(num1) - Number(num2));
 }
 
 subtractNums(3, 7);
 
 // Multiplication operation
 function multiplyNums(num1, num2) {
-  console.log(+num1 * +num2);
+  return (+num1 * +num2);
 }
 
 multiplyNums(2, 2);
 
 // Division operation
 function divideNums(num1, num2) {
-  console.log(+num1 / +num2);
+  return (+num1 / +num2);
 }
 
 divideNums(6, 3);
 
 // Power operation
 function powerNums(num1, num2) {
-  console.log((+num1) ** (+num2));
+  return ((+num1) ** (+num2));
 }
 
 powerNums(7, 2);
 
 // Mod operation
 function modNums(num1, num2) {
-  console.log(+num1 % +num2);
+  return (+num1 % +num2);
 }
 
 modNums(10, 2);
 
 // Square root operation
 function sqrtNum(num1) {
-  console.log(Math.sqrt(+num1));
+  return (Math.sqrt(+num1));
 }
 
 sqrtNum(25);
 
 
 
-function calculate(expression) {}
+function calculate(expression) {
+  const tokens = expression.split(' ');
+  
+  const num1 = Number(tokens[0]);
+  const operator = tokens[1];
+  const num2 = Number(tokens[2]);
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    alert ("You have entered an invalid number. Please try again.");
+    return;
+  }
+
+  if (operator === '+') {
+    return addNums(num1, num2);
+  }
+
+  if (operator === '-') {
+    return subtractNums(num1, num2);
+  }
+
+  if (operator === '*') {
+    return multiplyNums(num1, num2);
+  }
+
+  if (operator === '/') {
+    return divideNums(num1, num2);
+  }
+
+  if (operator === '**') {
+    return powerNums(num1, num2);
+  }
+
+  if (operator === '%') {
+    return modNums(num1, num2);
+  }
+
+  if (operator === 'Math.sqrt') {
+    return sqrtNum(num1);
+  }
+
+}
 
 /* **************** DO NOT EDIT THE CODE BELOW **************** */
 /* ************************************************************ */
